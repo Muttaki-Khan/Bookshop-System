45a520db03a436f1664ee14b903fdf289cca5c1e
Mahin <mahinarafatmeem@gmail.com>
2021-01-15 17:56:36 +0600

    Added edit profile function for admin

--- application/views/admin/admin_nav.php
+++ application/views/admin/admin_nav.php
@@ -10,7 +10,8 @@
     </div>
     <div class="admin-menu">
         <ul>
-            <li><a href="<?= base_url()?>admin"><i class="fas fa-home"></i> Dashboard</a></li>
+			<li><a href="<?= base_url('admin/edit-profile/'.$this->session->userdata('id').'')?>"><i class="fas fa-user"></i> Edit profile</a></li>
+			<li><a href="<?= base_url()?>admin"><i class="fas fa-home"></i> Dashboard</a></li>
             <li><a href="<?= base_url()?>admin/category"><i class="far fa-list-alt"></i> Category</a></li>
             <li><a href="<?= base_url()?>admin/books"><i class="fas fa-book"></i> Books</a></li>
             <li><a href="<?= base_url()?>admin/usedbooks"><i class="fas fa-book"></i>Used Books</a></li>
