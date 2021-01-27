8d4942372e22631d257d62c0b4098e7cdce90299
shahan-23 <shahanali23.sa.742@gmail.com>
2021-01-27 16:46:22 +0600

    main admin search fixed

--- application/views/admin/admin_nav.php
+++ application/views/admin/admin_nav.php
@@ -1,25 +1,22 @@
 <div class="admin-nav">
     <div class="user-name"><i class="fas fa-user"></i> <?php print $this->session->userdata('name') ?></div>
     <div class="admin-search">
-        <form action="">
-            <div class="form-group">
-                <input type="text" class="form-control" placeholder="Search...">
-                <button type="submit"><i class="fas fa-search"></i></button>
-            </div>
-        </form>
+        <?= form_open('users/search', ['id'=>'user-search'])?>
+        <input type="text" name="search_book" class="form-control" placeholder="Search any book">
+        <button type="submit"><i class="fas fa-search"></i></button>
+        <?= form_close()?>
     </div>
     <div class="admin-menu">
         <ul>
-			<li><a href="<?= base_url('admin/edit-profile/'.$this->session->userdata('id').'')?>"><i class="fas fa-user"></i> Edit profile</a></li>
-			<li><a href="<?= base_url()?>admin"><i class="fas fa-home"></i> Dashboard</a></li>
-            <li><a href="<?= base_url()?>admin/category"><i class="far fa-list-alt"></i> Category</a></li>
-            <li><a href="<?= base_url()?>admin/books"><i class="fas fa-book"></i> Books</a></li>
-            <li><a href="<?= base_url()?>admin/usedbooks"><i class="fas fa-book"></i>Used Books</a></li>
-            <li><a href="<?= base_url()?>admin/allusers"><i class="fas fa-users"></i> Users</a></li>
-            <li><a href="<?= base_url()?>admin/orders"><i class="fas fa-cart-arrow-down"></i> Orders</a></li>
-            <li><a href=""><i class="fas fa-layer-group"></i> Others</a></li>
-            <li><a href=""><i class="fas fa-cog"></i> Settings</a></li>
-            <li><a href="<?= base_url()?>users/logout"><i class="fas fa-power-off"></i> Logout</a></li>
-        </ul>
-    </div>
+           <li><a href="<?= base_url()?>admin"><i class="fas fa-home"></i> Dashboard</a></li>
+           <li><a href="<?= base_url()?>admin/category"><i class="far fa-list-alt"></i> Category</a></li>
+           <li><a href="<?= base_url()?>admin/books"><i class="fas fa-book"></i> Books</a></li>
+           <li><a href="<?= base_url()?>admin/usedbooks"><i class="fas fa-book"></i> Used Books</a></li>
+           <li><a href="<?= base_url()?>admin/allusers"><i class="fas fa-users"></i> Users</a></li>
+           <li><a href="<?= base_url()?>admin/orders"><i class="fas fa-cart-arrow-down"></i> Orders</a></li>
+           <li><a href="<?= base_url()?>admin/tborders"><i class="fas fa-cart-arrow-down"></i> 3B Sharing Orders</a></li>
+           <li><a href="<?= base_url('admin/edit-profile/'.$this->session->userdata('id').'')?>"><i class="fas fa-user"></i> Edit profile</a></li>
+           <li><a href="<?= base_url()?>users/logout"><i class="fas fa-power-off"></i> Logout</a></li>
+       </ul>
+   </div>
 </div>
